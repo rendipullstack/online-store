eefa9667593db24729acf19d603318a4dc588f1b
Mochammad Hanif <mochanifz@gmail.com>
2023-02-16 11:34:24 +0700

    [Update] added category struct

--- models/category_model.go
+++ models/category_model.go
@@ -9,3 +9,27 @@ type Category struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type CategoryInput struct {
+	Type string `json:"type" binding:"required"`
+}
+
+type CategoryResponsePost struct {
+	ID        uint      `json:"id"`
+	Type      string    `json:"type"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type CategoryResponseGet struct {
+	ID        uint              `gorm:"primarykey" json:"id"`
+	Type      string            `json:"type"`
+	CreatedAt time.Time         `json:"created_at"`
+	UpdatedAt time.Time         `json:"updated_at"`
+	Product   []ProductResponse `json:"products"`
+}
+
+type CategoryResponsePatch struct {
+	ID        uint      `json:"id"`
+	Type      string    `json:"type"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
