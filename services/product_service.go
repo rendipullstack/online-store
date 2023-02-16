11cac1ee88407af83bca863f348f1d67a2cbb860
Mochammad Hanif <mochanifz@gmail.com>
2023-02-16 11:32:05 +0700

    [Setup] product service

---
+++ services/product_service.go
@@ -0,0 +1,124 @@
+package services
+
+import (
+	"fmt"
+	"online-store/helpers"
+	"online-store/models"
+	"online-store/repositories"
+
+	"github.com/dustin/go-humanize"
+)
+
+type ProductServices interface {
+	Create(input models.ProductInput) (models.ProductResponse, error)
+	GetProducts() ([]models.ProductResponse, error)
+	DeleteProduct(productID uint) error
+	SaveProduct(input models.ProductInput, productID uint) (models.ProductResponseUpdate, error)
+}
+
+type productServices struct {
+	repository         repositories.ProductRepositories
+	categoryRepository repositories.CategoryRepositories
+}
+
+func NewProductServices(repository repositories.ProductRepositories, categoryRepository repositories.CategoryRepositories) *productServices {
+	return &productServices{repository, categoryRepository}
+}
+
+func (ps *productServices) Create(input models.ProductInput) (models.ProductResponse, error) {
+	var (
+		product         models.Product
+		productResponse models.ProductResponse
+	)
+
+	category, err := ps.categoryRepository.GetDataByID(input.CategoryID)
+	if err != nil {
+		return productResponse, err
+	}
+
+	product.Title = input.Title
+	product.Price = input.Price
+	product.Stock = &input.Stock
+	product.CategoryID = category.ID
+
+	product, err = ps.repository.PostProduct(product)
+
+	productResponse.ID = product.ID
+	productResponse.Title = product.Title
+	productResponse.Price = product.Price
+	productResponse.Stock = *product.Stock
+	productResponse.CategoryID = product.CategoryID
+	productResponse.CreatedAt = product.CreatedAt
+
+	return productResponse, helpers.ReturnIfError(err)
+}
+
+func (ps *productServices) GetProducts() ([]models.ProductResponse, error) {
+	var (
+		products         []models.Product
+		productResponses []models.ProductResponse
+	)
+
+	products, err := ps.repository.GetProducts()
+
+	for _, product := range products {
+		var productResponse models.ProductResponse
+
+		productResponse.ID = product.ID
+		productResponse.Title = product.Title
+		productResponse.Price = product.Price
+		productResponse.Stock = *product.Stock
+		productResponse.CategoryID = product.CategoryID
+		productResponse.CreatedAt = product.CreatedAt
+
+		productResponses = append(productResponses, productResponse)
+	}
+
+	return productResponses, helpers.ReturnIfError(err)
+}
+
+func (ps *productServices) DeleteProduct(productID uint) error {
+	product, err := ps.repository.GetDataByID(productID)
+	if err != nil {
+		return err
+	}
+
+	err = ps.repository.DeleteProducts(product)
+
+	return helpers.ReturnIfError(err)
+}
+
+func (ps *productServices) SaveProduct(input models.ProductInput, productID uint) (models.ProductResponseUpdate, error) {
+	var (
+		product         models.Product
+		productResponse models.ProductResponseUpdate
+	)
+
+	product, err := ps.repository.GetDataByID(productID)
+	if err != nil {
+		return productResponse, err
+	}
+
+	product.Title = input.Title
+	product.Price = input.Price
+	product.Stock = &input.Stock
+	product.CategoryID = input.CategoryID
+
+	product, err = ps.repository.UpdateProduct(product)
+
+	productData := models.ProductResponseData{
+		ID:         productID,
+		Title:      product.Title,
+		Price:      fmt.Sprintf("Rp %s", humanize.Comma(int64(product.Price))),
+		Stock:      *product.Stock,
+		CategoryID: product.CategoryID,
+		CreatedAt:  product.CreatedAt,
+		UpdatedAt:  product.UpdatedAt,
+	}
+
+	productResponse = models.ProductResponseUpdate{
+		Product: productData,
+	}
+
+	return productResponse, helpers.ReturnIfError(err)
+}
