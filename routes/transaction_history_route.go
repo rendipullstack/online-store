22ee86078f061b6863f121d514128691ac40aca2
Mochammad Hanif <mochanifz@gmail.com>
2023-02-18 18:25:58 +0700

    [Update] fixing route transaction history

--- routes/transaction_history_route.go
+++ routes/transaction_history_route.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TransactionRoute(c *gin.Engine, controller controllers.TransactionController) {
-	c.POST("/transactions", middlewares.AuthMiddleware, controller.CreateTransaction)
-	c.GET("/transactions/my-transactions", middlewares.AuthMiddleware, controller.GetUserTransactions)
+	c.POST("/checkout", middlewares.AuthMiddleware, controller.CreateTransaction)
+	c.GET("/transactions", middlewares.AuthMiddleware, controller.GetUserTransactions)
 	c.GET("/transactions/user-transactions", middlewares.AuthMiddleware, middlewares.AdminMiddleware, controller.GetAllTransactions)
 }
