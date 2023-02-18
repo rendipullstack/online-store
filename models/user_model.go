c2c376233082778ffa3737edf20ad0296cb38f01
Mochammad Hanif <mochanifz@gmail.com>
2023-02-18 18:24:38 +0700

    [Update] example struct user model

--- models/user_model.go
+++ models/user_model.go
@@ -13,14 +13,14 @@ type User struct {
 }
 
 type UserRegisterInput struct {
-	FullName string `json:"full_name" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required" example:"hanif"`
+	Email    string `json:"email" binding:"required,email" example:"me@hanifz.com"`
+	Password string `json:"password" binding:"required,min=6" example:"qweqwe"`
 }
 
 type UserLoginInput struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6"`
+	Email    string `json:"email" binding:"required,email" example:"me@hanifz.com"`
+	Password string `json:"password" binding:"required,min=6" example:"qweqwe"`
 }
 
 type UserResponseRegister struct {
