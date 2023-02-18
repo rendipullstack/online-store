d9b4c21e6a2d6548fe364999b5338aa553129a9f
Mochammad Hanif <mochanifz@gmail.com>
2023-02-18 19:11:35 +0700

    [Update] fixing host url api

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:8080
+// @host      online-store-hanif.up.railway.app
 // @BasePath  /
 
 // @securityDefinitions.apikey BearerAuth
