95b23b5464cffebee87e4fea4c5fb4ab8cdf913a
Mochammad Hanif <mochanifz@gmail.com>
2023-02-18 18:22:53 +0700

    [Update] added swaggo command

--- main.go
+++ main.go
@@ -11,8 +11,34 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+
+	_ "online-store/docs"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title           Online Store Challenge API
+// @version         1.0
+// @description     Software Engineer Challenge PT. Synapsis Sinergi Digital.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Mochammad Hanif
+// @contact.url    http://www.github.com/nifz
+// @contact.email  ochammadhanif@gmail.com
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
 func main() {
 	_ = godotenv.Load()
 
@@ -40,6 +66,10 @@ func main() {
 	productService := services.NewProductServices(productRepository, categoryRepository)
 	productController := controllers.NewProductController(productService)
 
+	cartRepository := repositories.NewCartRepository(db)
+	cartService := services.NewCartService(cartRepository, productRepository, userRepository, categoryRepository)
+	cartController := controllers.NewCartController(cartService)
+
 	transactionRepository := repositories.NewTransactionRepository(db)
 	transactionService := services.NewTransactionService(transactionRepository, productRepository, userRepository, categoryRepository)
 	transactionController := controllers.NewTransactionController(transactionService)
@@ -48,7 +78,8 @@ func main() {
 	routes.UserRoute(router, userController)
 	routes.CategoryRoutes(router, categoryController)
 	routes.ProductRoutes(router, productController)
+	routes.CartRoute(router, cartController)
 	routes.TransactionRoute(router, transactionController)
-
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.Run(":" + port)
 }
