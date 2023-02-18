b3baab2f9737f21511338ad7ae82d02c8cedc003
Mochammad Hanif <mochanifz@gmail.com>
2023-02-18 18:20:29 +0700

    [Update] added auto migration

--- configs/database.go
+++ configs/database.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"online-store/helpers"
+	"online-store/models"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -12,9 +13,9 @@ import (
 func GetDatabase(username, password, host, port, dbName string) (*gorm.DB, error) {
 	dbURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, dbName)
 
-	fmt.Println(dbURL)
-
 	db, err := gorm.Open(mysql.Open(dbURL), &gorm.Config{})
+
+	db.AutoMigrate(&models.User{}, &models.Product{}, &models.Category{}, &models.Cart{}, &models.TransactionHistory{})
 
 	return db, helpers.ReturnIfError(err)
 }
